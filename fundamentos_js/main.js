a00e1708118397d027e9b1aa65e17dca7f4d5674
aland3v <aland3v@gmai.com>
2020-10-23 09:39:50 -0400

    Funciones como parametros

---
+++ fundamentos_js/main.js
@@ -0,0 +1,35 @@
+/**
+ * CURSO FUNDAMENTOS DE JAVASCRIPT DE PLATZI
+ */
+var alan = {
+  nombre: 'Alan',
+  apellido: 'Quispe',
+  edad: 27
+}
+
+var jorge = {
+  nombre: 'Jorge',
+  apellido: 'Morales',
+  edad: 28
+}
+
+
+function nombreMayuscula(persona){
+  var nombre = persona.nombre.toUpperCase()
+  console.log(nombre)
+}
+
+/** 
+ * Seguros que siempre vamos a recibir un objeto
+ *  con el atributo apellido.
+ */
+function apellidoMayuscula({ apellido }){
+  console.log(apellido.toUpperCase())
+}
+
+nombreMayuscula(alan)
+nombreMayuscula(jorge)
+apellidoMayuscula(alan)
+apellidoMayuscula(jorge)
+apellidoMayuscula({ apellido: 'Gomez'})
+apellidoMayuscula({ apellido: 'Fernandez', edad:'27'})
