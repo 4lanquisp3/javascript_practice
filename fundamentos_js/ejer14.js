e04946d08f8720f392b255d0b4a35b516d8bcda8
aland3v <aland3v@gmail.com>
2020-10-26 15:56:04 -0400

    Se crea una funcion asincrona

--- fundamentos_js/ejer14.js
+++ fundamentos_js/ejer14.js
@@ -18,53 +18,32 @@ function onError(id){
   console.log(`Sucedió un error al obtener el personaje ${id}`)
 }
 
-var ids = [1, 2, 3, 4, 5, 6, 7]
+/**
+ * Para poder utilizar await la funcion tenemos que 
+ * marcarla como Asincrona, con la palabra async antes de function
+ */
+async function obtenerPersonajes(){
+  var ids = [1, 2, 3, 4, 5, 6, 7]
+  var promesas = ids.map(id => obtenerPersonaje(id))
+  
+  /**
+   * Cuando todas las promesas se resuelvan, ese resultado
+   * guardamelo en "personajes", la ejecución de esta función
+   * se va a detener aka, hasta q todas las promesas sean resueltas
+   * y recien ahí ese valor se va guardar en la variable personajes 
+   * para que funcione debemos encerrarlo entre try-catch.
+   */
+  try{
+    var personajes = await Promise.all(promesas)
+    console.log(personajes)
+  }catch(id){
+    onError(id)
+  }  
+}
 
-/* var promesas = ids.map(function(id){
-  return obtenerPersonaje(id)
-}) */
-
-var promesas = ids.map(id => obtenerPersonaje(id))
+obtenerPersonajes()
 
 /**
- * Promise.all, metodo que tiene la clase de promesas
- * que recibe como argumento un array de promesas.
- */
-Promise
-  .all(promesas)
-  .then(personajes => console.log(personajes)) // personajes -> Array con los valores q 
-                                               // esperamos de cada una de las promesas
-  .catch(onError) // Se ejecuta si cualquiera de las promesas fallan
-
-/* obtenerPersonaje(1)
-  .then(personaje => { // "personaje" es data que nos llega de resolve(data)
-    console.log(`El personaje 1 es ${personaje.name}`)
-    return obtenerPersonaje(2)
-  })
-  .then(personaje => {
-    console.log(`El personaje 2 es ${personaje.name}`)
-    return obtenerPersonaje(3)
-  })
-  .then(personaje => {
-    console.log(`El personaje 3 es ${personaje.name}`)
-    return obtenerPersonaje(4)
-  })
-  .then(personaje => {
-    console.log(`El personaje 4 es ${personaje.name}`)
-    return obtenerPersonaje(5)
-  })
-  .then(personaje => {
-    console.log(`El personaje 5 es ${personaje.name}`)
-    return obtenerPersonaje(6)
-  })
-  .then(personaje => {
-    console.log(`El personaje 6 es ${personaje.name}`)
-    return obtenerPersonaje(7)
-  })
-  .then(personaje => {
-    console.log(`El personaje 7 es ${personaje.name}`)
-  })
-  // Algo bueno: Si en cualquiera de las promesas sucede algun error
-  // el catch es el mismo para todos
-  .catch(onError) // onError recibe id, el id de reject(id)
+ * Nota.- mientras la función Asincrona espera en la instrucción await, JS
+ * estará ejecutando normalmente otras instrucciones fuera de la funcion asincrona.
  */
